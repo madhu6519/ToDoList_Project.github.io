303dbb4027a32b96cf0a5d53a73bd2e4b5a0a9ab
Madhuri Ganapati <64521759+madhu6519@users.noreply.github.com>
2023-09-28 19:05:36 +0530

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+//creating inputBox constant element which holds task name
 const inputBox=document.getElementById("inputbox");
+
+//creating listContainer constant element which holds task as list item
 const listContainer=document.getElementById("list-container");
 
+//addTask is a function which is called when user click on Add button
 function addTask(){
-    if(inputBox.value === ''){
+    if(inputBox.value === ''){ 
+        //showing an alert message when no task available on todo list
         alert("You must write something!");
     }
     else{
         let li=document.createElement("li");
+        //shows list item values 
         li.innerHTML=inputBox.value;
         listContainer.appendChild(li);
         let span=document.createElement("span");
+        //this is for cross button
         span.innerHTML="\u00d7";
         li.appendChild(span);
     }
@@ -17,16 +24,21 @@ function addTask(){
 }
 
 listContainer.addEventListener("click",function(e){
+    //when user click on toggle it will checked that task or uncheck that task
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("check");
+        //calling saveData function to save content
         saveData();
     }
+    //when user click on cross symbol it's going remove item from todo list
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        //calling saveData function to save content
         saveData();
     }
 }, false);
 
+//save data on browser if user will refresh the page content remain on page
 function saveData(){
     localStorage.setItem("data",listContainer.innerHTML);
 }
